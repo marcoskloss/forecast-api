8243d740e3e6dccf77a6374766071b852d41185f
marcoskloss <marcoskloss2001@gmail.com>
2021-07-03 21:30:32 -0300

    add body-parser because supertest needs it

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import { Application } from 'express';
+import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -10,13 +12,19 @@ export class SetupServer extends Server {
   }
 
   public async init(): Promise<void> {
-    this.setupController();
+    this.setupExpress();
     await this.databaseSetup();
   }
 
-  private setupController(): void {
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.setupControllers();
+  }
+
+  private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   private async databaseSetup(): Promise<void> {
