c78a023b6fde4d92c7dfd6e9cd56140b3aca54c0
marcoskloss <marcoskloss2001@gmail.com>
2021-07-09 21:42:25 -0300

    add auth token generation

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
 
@@ -14,5 +15,28 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreatedUpdatedErrorResponse(res, error);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<Response | void> {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ code: 401, error: 'User not found!' });
+    }
+
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      return res.status(401).json({
+        code: 401,
+        error: 'Password does not match!',
+      });
+    }
+
+    const token = AuthService.generateToken(user.toJSON());
+    res.status(200).json({ token });
   }
 }
