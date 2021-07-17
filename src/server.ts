b0fbd0846d1bfb0f328319f554695e6320b4815e
marcoskloss <marcoskloss2001@gmail.com>
2021-07-17 17:29:24 -0300

    add cors and logs

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 import logger from './logger';
+import expressPino from 'express-pino-logger';
+import cors from 'cors';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -15,12 +17,22 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    this.setupControllers();
     await this.databaseSetup();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
-    this.setupControllers();
+    this.app.use(
+      expressPino({
+        logger,
+      })
+    );
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
   }
 
   private setupControllers(): void {
